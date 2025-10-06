5e56bbb4adb38fcb5b2b48579c4e5cbc3bf14cfa
Lo <l709937065@gmail.com>
2025-10-07 01:20:05 +0800

    chore(server): update wrangler configuration to use routes array and enable observability

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from "@ayingott/eslint-config"
 
 export default defineConfig({
   typescript: true,
-}).append({
-  files: ["packages/zzz-data/data/**/*.json"],
-  rules: {
-    "antfu/consistent-list-newline": "off",
-  },
 })
+  .append({
+    files: ["packages/zzz-data/data/**/*.json"],
+    rules: {
+      "antfu/consistent-list-newline": "off",
+    },
+  })
+  .append({ ignores: ["packages/server/worker-configuration.d.ts"] })
